4b1e8166c1b0b759032ae4750e498b97f5d1e0de
suwei <william.su@zenlayer.com>
2021-07-06 19:50:59 +0800

    Updates

---
+++ precache-manifest.18cf3e48f5e14fdcfeac8749d661cc44.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ac46cce16f3668449a9",
+    "url": "./static/css/main.9372aa98.chunk.css"
+  },
+  {
+    "revision": "2ac46cce16f3668449a9",
+    "url": "./static/js/main.2ac46cce.chunk.js"
+  },
+  {
+    "revision": "99a6362e960f778adff1",
+    "url": "./static/css/1.928bcf69.chunk.css"
+  },
+  {
+    "revision": "99a6362e960f778adff1",
+    "url": "./static/js/1.99a6362e.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "6119cbcde9267654ef9395ed1f8a7e59",
+    "url": "./static/media/suwei.6119cbcd.jpg"
+  },
+  {
+    "revision": "37d3af884ff90ff0aa2c1900f8aadbca",
+    "url": "./static/media/i_age.37d3af88.svg"
+  },
+  {
+    "revision": "f0e3a5265a5dcfe47dd261a75078d589",
+    "url": "./static/media/i_edu.f0e3a526.svg"
+  },
+  {
+    "revision": "8b97b54a5e3b94d0323aa56914e3751e",
+    "url": "./static/media/i_site.8b97b54a.svg"
+  },
+  {
+    "revision": "9d1fafb35f88ac771d98daeeed7d84b1",
+    "url": "./static/media/i_status.9d1fafb3.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "128994e9d0e9774a9ef2c85319dad70c",
+    "url": "./static/media/js.128994e9.webp"
+  },
+  {
+    "revision": "53fcb3371d141c6baaadf9f6cfde25e0",
+    "url": "./static/media/html.53fcb337.webp"
+  },
+  {
+    "revision": "8fe7badc2727f5d59defad0495d6dae3",
+    "url": "./static/media/cssPicture.8fe7badc.webp"
+  },
+  {
+    "revision": "f429903c59f904bb59e00672e49da13a",
+    "url": "./static/media/nodePic.f429903c.webp"
+  },
+  {
+    "revision": "fd0f493269d5eef97a65b34f5e190c05",
+    "url": "./static/media/w_link.fd0f4932.svg"
+  },
+  {
+    "revision": "2cd92e65205351f3e090798c54b19b81",
+    "url": "./static/media/SDWAN.2cd92e65.webp"
+  },
+  {
+    "revision": "70aa42a156e763ccd80df408693b76cc",
+    "url": "./static/media/SDN.70aa42a1.webp"
+  },
+  {
+    "revision": "bc85cfe6e91ae6e9b1c08a95f1ccef32",
+    "url": "./static/media/leetcode.bc85cfe6.webp"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "./static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "./static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "./static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "./static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "a6954892064683b461bf7dfc220a7205",
+    "url": "./index.html"
+  }
+];
